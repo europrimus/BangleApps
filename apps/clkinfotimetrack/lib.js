92f6edc448fb590ef030619d06456f01eda82157
Europrimus <dev@c-f.me>
2024-11-08 16:18:32 +0100

    use a different log file per week

--- apps/clkinfotimetrack/lib.js
+++ apps/clkinfotimetrack/lib.js
@@ -1,7 +1,28 @@
 exports = {
-    logFile: function(){return "clkinfotimetrack.log.csv"},
+    logFile: function(){
+        // the maximum length for filenames is 28 characters
+        // clkinfotimetrack_YYYY-WW.csv : 28 chars
+        const baseName="clkinfotimetrack_";
+        const ext=".csv";
+        year = (new Date()).getFullYear();
+        week = this.calcWeek();
+        let name = baseName + year + '-' + week + ext;
+        return name;
+    },
     confFile:function(){return "clkinfotimetrack.conf.json"},
     debug:false,
+
+    calcWeek: function(date) {
+        if(date === undefined){
+            date = new Date();
+        }
+        // new Date(year, monthIndex, day); fist month is 0
+        var firstJanuary = new Date(date.getFullYear(),0,1);
+        var today = new Date(date.getFullYear(),date.getMonth(),date.getDate());
+        // one day : 86400 : 24h x 60m x 60s
+        var dayOfYear = ((today - firstJanuary + 86400000)/86400000);
+        return Math.ceil(dayOfYear/7);
+    },
 
     defaultConf: function(){
         return {"taskName":[],"debug":false}
